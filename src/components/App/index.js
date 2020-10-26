ddb50aa38c07e0ff5c33819a41d04e7bf1b70f14
Axel <axel.baudain@gmail.com>
2020-10-26 11:30:09 +0100

    refactorized

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,14 @@
+// Library imports
 import React from 'react';
 import './style.scss';
+
+// Components imports
+import HomePage from '../HomePage';
 
 function App({ incrementProgressBar, progression }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <p className="header__title">
-            Axel Baudain <br />
-            Développeur web Fullstack
-          </p>
-        </div>
-        <button className="loading__name">
-          Me découvrir
-        </button>
-      </header>
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage/index.js
@@ -0,0 +1,21 @@
+// Library imports
+import React from 'react';
+
+// CSS imports
+import './style.scss';
+
+const HomePage = () => (
+  <header className="homepage__header">
+    <div>
+      <p className="header__title">
+        Axel Baudain <br />
+        Développeur web Fullstack
+      </p>
+    </div>
+    <button className="btn-discover">
+      Me découvrir
+    </button>
+  </header>
+);
+
+export default HomePage;
