75a046200a4c23f7f867aa7d1d7a33d1cec0b96e
Axel <axel.baudain@gmail.com>
2020-10-29 17:21:18 +0100

    Minor bugfix

--- src/components/Projects/SingleProject/index.js
+++ src/components/Projects/SingleProject/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.scss';
 
-const SingleProject = ({ image, desc = "", link = "", title = "A venir ...", available }) => {
+const SingleProject = ({ image, desc = "", link = "#", title = "A venir ...", available }) => {
 const classToApply = available ? "fa-angle-double-right" : "fa-times unvavailable"
 const btnClass = available ? "available" : "unavailable";
 
@@ -13,7 +13,7 @@ const btnClass = available ? "available" : "unavailable";
           {title}
         </div>
         {desc}
-        <a className={`btn-toproject ${btnClass}`} href={link} target="_blank" rel="noopener noreferrer" ><i class={`fas ${classToApply} fa-2x`} ></i></a>
+        <a className={`btn-toproject ${btnClass}`} href={link}><i class={`fas ${classToApply} fa-2x`} ></i></a>
       </div>
     </div>
   );
