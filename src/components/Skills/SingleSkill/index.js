39732a374458ae56f3f9097c49f4b0a7ad5d493b
Axel <axel.baudain@gmail.com>
2020-10-29 16:39:19 +0100

    Added percent bars and associated skills

--- src/components/Skills/SingleSkill/index.js
+++ src/components/Skills/SingleSkill/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './style.scss';
 
-const SingleSkill = () => (
-  <div>
-    Skill X
+const SingleSkill = ({ skillname, percent }) => (
+  <div className="single__skill">
+    <div className="skill__name">
+      {skillname}
+    </div>
+    <div className="progression">
+      <div className="progression__bar" style={{ width: `${percent}%`}} />
+    </div>
+    <div className="skill__percent">
+      {percent}%
+    </div>
   </div>
 );
 
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -9,43 +9,61 @@ const Skills = () => (
   </div>
     <div className="skills__list">
       <div className="list__title">
-        Titre skills
+        Systèmes d'exploitation
       </div>
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
+      <SingleSkill skillname="Windows 10" percent={95} />
+      <SingleSkill skillname="Ubuntu (Linux)" percent={75} />
     </div>
     <div className="skills__list">
       <div className="list__title">
-        Titre skills
+        Langages
       </div>
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
+      <SingleSkill skillname="HTML 5" percent={75} />
+      <SingleSkill skillname="CSS 3" percent={80} />
+      <SingleSkill skillname="PHP 7+" percent={70} />
+      <SingleSkill skillname="JavaScript" percent={95} />
+      <SingleSkill skillname="SQL" percent={60} />
     </div>
     <div className="skills__list">
       <div className="list__title">
-        Titre skills
+        Frameworks
       </div>
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
+      <SingleSkill skillname="React-JS" percent={90} />
+      <SingleSkill skillname="Redux" percent={85} />
+      <SingleSkill skillname="SemanticUI" percent={55} />
+      <SingleSkill skillname="Bootstrap (CSS)" percent={60} />
+      <SingleSkill skillname="AltoRouter (PHP)" percent={85} />
+      <SingleSkill skillname="Lumen (PHP)" percent={50} />
     </div>
     <div className="skills__list">
       <div className="list__title">
-        Titre skills
+        Tests unitaires
       </div>
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
-      <SingleSkill />
+      <SingleSkill skillname="TDD" percent={30} />
+      <SingleSkill skillname="Mocha-JS" percent={75} />
+      <SingleSkill skillname="Chai-JS" percent={70} />
+      <SingleSkill skillname="Enzyme-JS" percent={50} />
+    </div>
+    <div className="skills__list">
+      <div className="list__title">
+        Base de données
+      </div>
+      <SingleSkill skillname="MySQL" percent={60} />
+      <SingleSkill skillname="MariaDB" percent={60} />
+    </div>
+    <div className="skills__list">
+      <div className="list__title">
+        Outil de versionning
+      </div>
+      <SingleSkill skillname="GIT" percent={75} />
+    </div>
+    <div className="skills__list">
+      <div className="list__title">
+        Skills annexes
+      </div>
+      <SingleSkill skillname="Whimsical" percent={80} />
+      <SingleSkill skillname="Draw.IO" percent={70} />
+      <SingleSkill skillname="Adobe Illustrator" percent={25} />
     </div>
   </div>
 );
