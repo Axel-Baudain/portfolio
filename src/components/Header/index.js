135e07f0d624f99965c4826c61c3a2c06873b3d3
Axel-Baudain <axel.baudain@gmail.com>
2020-12-18 08:33:22 +0100

    Building a new section - story

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,6 +11,7 @@ const Header = () => (
     <div className="links">
       <NavLink className="header__link" exact to="/"><i class="fas fa-home" /></NavLink>
       <NavLink className="header__link" exact to="/about"><i class="far fa-user" /></NavLink>
+      <NavLink className="header__link" exact to="/story"><i class="fas fa-map-signs" /></NavLink>
       <NavLink className="header__link" exact to="/projects"><i class="fas fa-book" /></NavLink>
       <NavLink className="header__link" exact to="/skills"><i class="fas fa-tasks" /></NavLink>
       <NavLink className="header__link" exact to="/contact"><i class="far fa-address-card" /></NavLink>
